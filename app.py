cdac40a6106622e68e8d9ba548f123165b664873
sosullivan7221 <sosullivan7221@gmail.com>
2023-09-17 16:30:37 +0000

    consolidated home/data pages

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('home.html')
-
-
 df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA_507_2023/main/WK3/data/trinetx/medication_drug.csv')
-@app.route('/data')
+@app.route('/')
 def dataset(data=df):
     data = data.sample(15)
     return render_template('data.html', data=data)
