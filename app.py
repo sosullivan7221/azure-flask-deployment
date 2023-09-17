9b722b3baa66b9ff2fa8b63c3c4651e435f51851
sosullivan7221 <sosullivan7221@gmail.com>
2023-09-17 17:39:43 +0000

    test

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 app = Flask(__name__)
 
-df = pd.read_csv('https://raw.githubusercontent.com/hantswilliams/HHA_507_2023/main/WK3/data/trinetx/medication_drug.csv')
+df = pd.read_csv('/home/sean_osullivan/azure-flask-deployment/data/medication_drug.csv')
 @app.route('/')
 def dataset(data=df):
     data = data.sample(15)
